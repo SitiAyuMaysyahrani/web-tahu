a5268453fc3f8c46f2bc8983a5a7210db0f48737
SitiAyuMaysyahrani <sitiayumaysyahranii@gmail.com>
2022-12-15 23:34:38 +0700

    first commit

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Siti Ayu Maysyahrani" // TODO: replace this
+const studentId = "FE2677944" // TODO: replace this
+
+export { url, studentName, studentId }
